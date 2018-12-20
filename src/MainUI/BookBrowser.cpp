7e403a2d0be6d2d473b5dc3dc881e80fcf364ce3
fuguoqiang <fuguoqiang@luojilab.com>
2018-12-20 19:37:53 +0800

    fix(book browser highlight-color): fix high light color of adding copy right page

--- src/MainUI/BookBrowser.cpp
+++ src/MainUI/BookBrowser.cpp
@@ -1,4 +1,4 @@
-﻿/************************************************************************
+/************************************************************************
 **
 **  Copyright (C) 2009, 2010, 2011  Strahinja Markovic  <strahinja.markovic@gmail.com>
 **
@@ -1800,10 +1800,10 @@ void BookBrowser::AddCopyrightPage()
     // Open the new file in a tab
     emit ResourceActivated(new_html_resource);
     emit BookContentModified();
-    Refresh();
     
     // rename
     m_OPFModel->RenameResource(new_html_resource, copyrightFileName);
+    Refresh();
 }
 
 void BookBrowser::AddFullScreenPage()
